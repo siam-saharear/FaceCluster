fe9e98ce03ff03ced037a5834f578046749a2a4a
zeher <siamsaharear010@gmail.com>
2026-08-17 01:54:21 +0600

    refactor face database matching into separate function

--- face_clustering.py
+++ face_clustering.py
@@ -13,29 +13,31 @@ def similarity(embedding_1, embedding_2, threshold=None):
     return False
 
 
+def find_matching_person(face_embedding, face_database):
+    for person in face_database:
+        if similarity(face_embedding, person["embedding"]):
+            return person
+        else:
+            return None 
+
+
 def build_face_database(image_paths, images_n_faces):
     face_database = []
     face_id = 0
     for image_path in image_paths:
         faces = images_n_faces[image_path]["faces"]
         for face in faces:
-            face_embedding = face.embedding
-            if len(face_database) == 0:
-                face_database.append({"id":face_id,
-                                    "embedding":face_embedding, 
-                                    "appearance":[{"path":image_path, "bbox":face.bbox, "face":face}]})
+            person = find_matching_person(face.embedding,face_database)
+            appearance = {"path":image_path,
+                          "bbox": face.bbox,
+                          "face": face}
+            if person:
+                person["appearance"].append(appearance)
+            else:
+                face_database.append({
+                    "id":face_id,
+                    "embedding":face.embedding,
+                    "appearance":[appearance]
+                })
                 face_id += 1
-                continue
-            matched = False
-            for person in face_database:
-                compare_embedding = person["embedding"]
-                if similarity(face_embedding, compare_embedding):
-                    person["appearance"].append({"path":image_path, "bbox":face.bbox, "face":face})
-                    matched = True
-                    break
-            if not matched:
-                face_database.append({"id":face_id,
-                                        "embedding":face_embedding,
-                                        "appearance":[{"path":image_path, "bbox":face.bbox, "face":face}]})
-                face_id+=1
     return face_database
